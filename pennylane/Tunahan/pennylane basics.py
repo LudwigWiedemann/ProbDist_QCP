e20e94d65c1d6cb052171ec4eb0be618aca2d406
Tsunari <124319161+Tsunari@users.noreply.github.com>
2024-05-22 08:32:38 +0200

    main.py is cool rest doesnt work so cool

--- pennylane/Tunahan/pennylane basics.py
+++ pennylane/Tunahan/pennylane basics.py
@@ -2,13 +2,14 @@ import pennylane as qml
 
 dev = qml.device("default.qubit", wires=[0, 1])
 
-def my_first_circuit(theta):
 
+def my_first_circuit(theta):
     qml.Hadamard(wires=0)
     qml.CNOT(wires=[0, 1])
     qml.RY(theta, wires=0)
 
     return qml.probs(wires=[0, 1])
+
 
 my_first_QNode = qml.QNode(my_first_circuit, dev)
 
