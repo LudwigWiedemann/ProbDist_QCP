0e9bdf5e3a690b6f6201de7d67c35e834ffc6a8d
wiedemannlu <wiedemannlu@cip.ifi.lmu.de>
2024-05-29 00:43:31 +0200

    add shots

--- pennylane/Ludwig/training.py
+++ pennylane/Ludwig/training.py
@@ -4,32 +4,37 @@ import circuit as cir
 import plotting as plot
 
 optimizer = qml.GradientDescentOptimizer(0.001)
-training_iterations = 100
+training_iterations = 50
 
 
 def f(x):
     return np.sin(x)
-    #  return np.sin(x) + 0.5*np.cos(2*x) + 0.25 * np.sin(3*x)
+    # return np.sin(x) + 0.5*np.cos(2*x) + 0.25 * np.sin(3*x)
 
 
-def train_params(training_data):
-    params = guess_starting_params(9)
-    print("Training the circuit...")
-    for iteration in range(training_iterations):
-        for training_x, training_y in training_data:
-            params = optimizer.step(cost, params, x=training_x, target=training_y)
-        total_error = 0
-        if iteration % 10 == 0:
-            print(f"Iteration {iteration}:")
-            for training_x, training_y in training_data:
-                predicted_output = cir.run_circuit(params, training_x)
-                error = np.abs(predicted_output - training_y)
-                total_error += error
-                # print( f"Input: {training_x}, Expected: {training_y:.4f}, Predicted: {predicted_output:.4f},
-                # Error: {error:.4f}")
-            print("total: " + str(total_error) + "average: " + str(total_error / len(training_data)))
-            plot.plot(params, training_data, f)
-    return params
+def train_params(distributions):
+    param_list = [[]] * len(distributions)
+    i = 0
+    for dist in distributions:
+        params = guess_starting_params(9)
+        print("Training the circuit...")
+        for iteration in range(training_iterations):
+            for training_x, training_y in dist:
+                params = optimizer.step(cost, params, x=training_x, target=training_y)
+            total_error = 0
+            if iteration % 10 == 0:
+                print(f"Iteration {iteration}:")
+                for training_x, training_y in dist:
+                    predicted_output = cir.run_circuit(params, training_x)
+                    error = np.abs(predicted_output - training_y)
+                    total_error += error
+                    # print( f"Input: {training_x}, Expected: {training_y:.4f}, Predicted: {predicted_output:.4f},
+                    # Error: {error:.4f}")
+                print("total: " + str(total_error) + "average: " + str(total_error / len(dist)))
+                # plot.plot([params], [dist], f)
+        param_list[i] = params
+        i += 1
+    return param_list
 
 
 def cost(params, x, target):
