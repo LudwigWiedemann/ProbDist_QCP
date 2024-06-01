3c17792d30d9d22f6a8cc2d45551476e84d6a77f
BPaty <93763932+BPaty@users.noreply.github.com>
2024-06-01 19:56:13 +0200

    Pipeline start

--- pennylane/working_directory/pipelines.py
+++ pennylane/working_directory/pipelines.py
@@ -3,10 +3,13 @@ import numpy as np
 import noise as ns
 import training as tr
 import plotting as plot
+import easygui as eg
+import json
 
-num_training_points = 60
+num_training_points = 20
 start = 0
 stop = 10
+training_inputs = np.linspace(start, stop, num_training_points)
 
 
 
@@ -23,45 +26,65 @@ def prepare_data(num_shots):
 
 
 def run(num_shots):
+    print("run")
     training_distributions = prepare_data(num_shots)
     param_list = tr.train_params(training_distributions)
-    plot.plot(param_list, training_distributions, tr.f)
+    plot.plot(param_list, training_distributions, tr.f, [[0, 'Y*x']])
 
 
 
-def questions():
-    num_training_points, start, stop = map(int, input("please enter trainingspoints start stop").split())
-    if stop <= start:
-        raise ValueError("Stop has to be higher then start")
-    if num_training_points < 1:
-        raise ValueError("num_training_points has to be a positive integer")
-    shots = int(input("Please enter number of shots: "))
-    if shots < 1:
-        raise ValueError("Number of shots has to be a positive integer")
-    runs = input("Please enter the number of runs (default=1) or 'inf': ")
-    if not runs == 'inf':
-        if int(runs) < 1 or isinstance(runs, int):
-            raise ValueError("Shots has to be a positive integer or inf")
-    xyz_seed, arythmetic_seed = map(int, input("Please enter seeds for xyz, arythmetic: ").split())
-    if xyz_seed == "":
-        xyz_seed = "rand"
-    if arythmetic_seed == "":
-        arythmetic_seed = "rand"
-    return num_training_points, start, stop, shots, runs
 
+def check_inputs_valid(training_points, startpoint, stoppoint, layers, qbits, shotcount, runcountz, seed):
+    if training_points is not "":
+        training_points = int(float(training_points))
+        if int(float(training_points)) < 1:
+            raise ValueError("Trainingpoint-count has to be a positive integer")
+    if startpoint is "":
+        raise ValueError("Start cannot be None")
+    if stoppoint is "":
+        raise ValueError("Stop cannot be None")
+    elif startpoint >= stoppoint:
+        raise ValueError("Start must be less than Stop")
+    if qbits is not "" and int(float(layers)) < 1:
+        raise ValueError("Layer-count has to be a positive integer or empty")
+    if qbits is not "" and int(float(qbits)) < 1 :
+        raise ValueError("Qbit-count has to be a positive integer or empty")
+    if shotcount is "":
+        raise ValueError("Shot-count cannot be None")
+    elif int(float(shotcount)) < 1:
+        raise ValueError("Shot-count has to be a positive integer")
+    if runcountz is not "" and int(float(runcountz)) < 1:
+        raise ValueError("Run-count has to be a positive integer or empty for infinite runs")
+    seed_list = json.loads(seed)
+    for elements in seed_list:
+        layer, cir = elements.split
+        xyz, arithmetic = cir[0], cir[1:]
+        if layer is "":
+            raise ValueError("Seed is not correctly formated at layer")
+        elif int(float(layer)) < 0 or layer >= layers:
+            raise ValueError("Seed has to be between 1 and num_layers-1")
+        if xyz is "" or xyz not in ["X", "Y", "Z"]:
+            raise ValueError("Seed is not correctly formated at XYZ")
+        if arithmetic is "" or arithmetic not in ["*1", "+x", "-x", "*x", "**x"]:
+            raise ValueError("Seed is not correctly formated at arithmetic")
 
-while True:
-    try:
-        num_training_points, start, stop, num_shots, runs = questions()
-        break
-    except Exception as e:
-        print(e)
+output = eg.buttonbox("Choose a Run","",["Run random", "Run specific"])
 
-training_inputs = np.linspace(start, stop, num_training_points)
-print("run")
-if runs == "inf":
-    while True:
-        run(num_shots)
-else:
-    for i in range(int(runs)):
-        run(num_shots)
+match output:
+    case "Run random": nothing
+    case "Run specific":
+        title= "Inputs"
+        msg= "Please enter values, empty fields will be assigned randomly if possible"
+        fields=["Trainingpoint-count", "Startpoint", "Stoppoint", "Layer-count", "Qbit-count", "Shot-count", "Run-count", "Seed"]
+        default=[]
+        while True:
+            try:
+                num_training_points, start, stop, num_layers, num_qbits, shots, runs, seed = eg.multenterbox(msg, title, fields, default)
+                check_inputs_valid(num_training_points, start, stop,num_layers, num_qbits, shots, runs, seed)
+                break
+            except ValueError as e:
+                print(e)
+                msg=e
+                default= [num_training_points, start, stop, num_layers, num_qbits, shots, runs, seed]
+
+#[[0, 'X*1'], [0, 'Y+x'], [1, 'X*x'], [1, 'Y-x'], [2, 'X-x'], [2, 'Y-x'], [3, 'Y+x'], [5, 'X**x'], [5, 'Z**x'], [6, 'X*1'], [6, 'Y-x'], [8, 'X+x'], [8, 'Z*x']]
