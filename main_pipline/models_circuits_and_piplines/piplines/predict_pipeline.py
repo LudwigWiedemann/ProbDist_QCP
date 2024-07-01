bfa156fdc0b3bbb026ad59af332ef4ae3dc2a9fe
TomBintener <tom.bintener@gmail.com>
2024-07-01 15:21:10 +0200

    Add classical baseline

--- main_pipline/models_circuits_and_piplines/piplines/predict_pipeline.py
+++ main_pipline/models_circuits_and_piplines/piplines/predict_pipeline.py
@@ -1,5 +1,7 @@
-# predict_pipeline.py
 import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 import time
 from silence_tensorflow import silence_tensorflow
 import numpy as np
@@ -11,6 +13,7 @@ from main_pipline.input.div.dataset_manager import generate_time_series_data
 from main_pipline.models_circuits_and_piplines.circuits.variable_circuit import new_RYXZ_Circuit, new_baseline
 from main_pipline.models_circuits_and_piplines.circuits.amp_Circuit import base_Amp_Circuit, layered_Amp_Circuit, \
     tangle_Amp_Circuit, test_Amp_Circuit, double_Amp_Circuit
+from main_pipline.models_circuits_and_piplines.models.baseline_models.predict_classic_circuit_model import PCCModel
 from main_pipline.models_circuits_and_piplines.models.predict_variable_circuit_model import PVCModel
 from main_pipline.models_circuits_and_piplines.models.predict_amp_circuit_model import PACModel
 from main_pipline.models_circuits_and_piplines.models.baseline_models.predict_hybrid_model import PHModel
@@ -19,31 +22,33 @@ from main_pipline.models_circuits_and_piplines.piplines.predict_pipline_div.pred
 from main_pipline.models_circuits_and_piplines.piplines.predict_pipline_div.predict_plots_and_metrics import \
     show_all_evaluation_plots
 
+trial_name = 'Classic_Model'
+
 full_config = {
     # Dataset parameter
     'time_frame_start': -4 * np.pi,
     'time_frame_end': 12 * np.pi,
-    'n_steps': 80,
-    'time_steps': 8,
-    'future_steps': 4,
-    'num_samples': 80,
-    'noise_level': 0.1,
+    'n_steps': 256,
+    'time_steps': 64,
+    'future_steps': 6,
+    'num_samples': 256,
+    'noise_level': 0.2,
     'train_test_ratio': 0.6,
     # Plotting parameter
     'preview_samples': 3,
-    'show_dataset_plots': True,
+    'show_dataset_plots': False,
     'show_model_plots': False,
     'show_forecast_plots': True,
     'steps_to_predict': 300,
     # Model parameter
-    'model': 'Amp_circuit',
+    'model': 'PCCModel',
     'circuit': 'Tangle_Amp_Circuit',
     # Run parameter
-    'epochs': 1,
-    'batch_size': 32,
-    'learning_rate': 0.5,
+    'epochs': 600,
+    'batch_size': 55,
+    'learning_rate': 0.0005,
     'loss_function': 'mse',
-    'compress_factor': 1.5,
+    'compress_factor': 8.61,
     'patience': 10,
     'min_delta': 0.001,
     # Circuit parameter
@@ -52,9 +57,10 @@ full_config = {
 }
 
 models = {
-    'Hybrid': PHModel,
-    'Variable_circuit': PVCModel,
-    'Amp_circuit': PACModel
+    'PHModel': PHModel,
+    'PVCModel': PVCModel,
+    'PACModel': PACModel,
+    'PCCModel': PCCModel
 }
 
 circuits = {
@@ -97,20 +103,20 @@ def run_model(dataset, config, logger):
 
 
 def main():
-    trial_folder = filemanager.create_folder("trial_main")
+    trial_folder = filemanager.create_folder(trial_name)
     logger = Logger(trial_folder)
 
     logger.info("Generating training data")
-    dataset = generate_time_series_data(function, full_config)
+    dataset = generate_time_series_data(function, full_config, logger)
     logger.info("Training data generated")
 
     model, loss = run_model(dataset, full_config, logger)
 
-    iterative_forecast(function, model, dataset, full_config, logger)
+    iterative_forecast(function, model, dataset, full_config, logger=logger)
     logger.info(f"Pipeline complete in {time.time() - start_time} seconds")
+
 
 if __name__ == "__main__":
     start_time = time.time()
     silence_tensorflow()
     main()
-
